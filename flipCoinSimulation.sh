51b40213ccb4e37c22ec4eced4db967dee1bd7a7
s8189882 <s8189882@gmail.com>
2022-08-03 18:29:09 +0530

    Displaying welcome message

---
+++ flipCoinSimulation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+#
+#Start with Displaying Welcome to Employee Wage Computation Program on Master #Branch
+#
+#
+
+
+echo ""
+echo "Welcome to Flip Coin Simulation Program "
+echo "This problem displays the winner Heads or Tails"
+echo ""
+
