b709cf99fec5b17e15067a414d64b8b6c45aaf04
s8189882 <s8189882@gmail.com>
2022-08-03 19:17:14 +0530

    Continue the coin flips until the tie broken is broken by difference of minimum 2 points

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -8,8 +8,7 @@ echo "This problem displays the winner Heads or Tails"
 echo ""
 
 
-# Modify the earlier UC 2 to continue till either of
-#them have won 21 times. Show if it’s a Win or Tie. If Win then who won and by how much
+#Extend UC 3 to ensure if its tie then the game continues till the difference of minimum 2 points is achieved
 t=0; h=0;
 while [[ $h -le 21 && $t -le 21 ]]; do
 	flip=$((RANDOM%2))
@@ -22,13 +21,25 @@ while [[ $h -le 21 && $t -le 21 ]]; do
 	fi
 done
 
+if [[ $h -eq $t ]]; then
+	echo "It's a tie!"
+	echo "Tie breaker Round"
+	while [[ $((h-$t)) -ne 2 && $((t-$h)) -ne 2 ]]; do
+	flip=$((RANDOM%2))
+	if [[ ${toss} -eq 0 ]]; then
+    	echo HEADS
+    	h=$((h+1))
+	elif [[ ${toss} -eq 1 ]]; then
+    	echo TAILS
+    	t=$((t+1))
+	fi
+done
+fi
 
 if [[ $t > $h ]]; then
 	echo "TAILS won by $((t-$h)) times."
 elif [[ $t < $h  ]]; then
 	echo "HEADS won by $((h-$t)) times."
-elif [[ $h -eq $t ]]; then
-	echo "It's a tie!"
 fi
 
 echo " HEADS : $h times     TAILS : $t times"
