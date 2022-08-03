90c89940393cae6eb03aac21de5ca98d3aae4d71
s8189882 <s8189882@gmail.com>
2022-08-03 19:05:56 +0530

    Flip till either heads or tails won 21 times - Displays if it’s a Win or Tie or won by how much

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -x
-#
-#Start with Displaying Welcome to Employee Wage Computation Program on Master #Branch
-#
-#
 
+#Start with Displaying Welcome to Employee Wage Computation Program on Master #Branch
 
 echo ""
 echo "Welcome to Flip Coin Simulation Program "
@@ -11,20 +8,30 @@ echo "This problem displays the winner Heads or Tails"
 echo ""
 
 
-#As a simulator, loop through Multiple times of flipping a coin and show number of times head and tail has won
-
-for (( i = 0; ; i++ )); do
+# Modify the earlier UC 2 to continue till either of
+#them have won 21 times. Show if it’s a Win or Tie. If Win then who won and by how much
+t=0; h=0;
+while [[ $h -le 21 && $t -le 21 ]]; do
 	flip=$((RANDOM%2))
-	if [[ ${flip} -eq 0 ]]; then
-		echo HEADS
-		h=$((h+1))
-	elif [[ ${flip} -eq 1 ]]; then
-		echo TAILS
-		t=$((t+1))
+	if [[ ${toss} -eq 0 ]]; then
+    	echo HEADS
+    	h=$((h+1))
+	elif [[ ${toss} -eq 1 ]]; then
+    	echo TAILS
+    	t=$((t+1))
 	fi
-	echo " HEADS : $h times     TAILS : $t times"
 done
 
+
+if [[ $t > $h ]]; then
+	echo "TAILS won by $((t-$h)) times."
+elif [[ $t < $h  ]]; then
+	echo "HEADS won by $((h-$t)) times."
+elif [[ $h -eq $t ]]; then
+	echo "It's a tie!"
+fi
+
+echo " HEADS : $h times     TAILS : $t times"
 echo ""
 
 
